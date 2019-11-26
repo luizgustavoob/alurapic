6dddd31c8bbfd3ac12a53b1360718b844e9ce85d
Luiz Gustavo <oluizgustavob@gmail.com>
2019-11-26 08:20:52 -0300

    nova guarda de rotas

--- alurapic/src/app/core/auth/login.guard.ts
+++ alurapic/src/app/core/auth/login.guard.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { UserService } from '../user/user.service';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from '../user/user.service';
 
 @Injectable({
     providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class LoginGuard implements CanActivate {
     
     constructor(private userService: UserService, private router: Router) { }
     
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
-        if (!this.userService.isLogged()) {
-            this.router.navigate(['']);
+        if (this.userService.isLogged()) {
+            this.router.navigate(['user', this.userService.getUserName()]);
             return false;
         }
         return true;
