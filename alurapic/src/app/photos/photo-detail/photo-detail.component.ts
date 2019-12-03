44091f549f104e7fb2f79140be224c099c12355e
Luiz Gustavo <oluizgustavob@gmail.com>
2019-12-03 08:39:58 -0300

    componente de notificação

--- alurapic/src/app/photos/photo-detail/photo-detail.component.ts
+++ alurapic/src/app/photos/photo-detail/photo-detail.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { PhotoService } from '../photo.service';
 import { Photo } from '../photo/photo';
 import { Observable } from 'rxjs';
+import { AlertService } from 'src/app/shared/components/alert/alert.service';
+import { UserService } from 'src/app/core/user/user.service';
 
 @Component({
     selector: 'app-photo-detail',
@@ -13,16 +15,30 @@ export class PhotoDetailComponent implements OnInit {
     photo$: Observable<Photo>;
     photoId: number;
     
-    constructor(private activateRoute: ActivatedRoute, private photoService: PhotoService, private router: Router){}
+    constructor(private activateRoute: ActivatedRoute, 
+                private photoService: PhotoService, 
+                private router: Router,
+                private alertService: AlertService,
+                private userService: UserService){}
     
     ngOnInit(): void {
         const id = this.activateRoute.snapshot.params.id;
         this.photoId = id;
-        this.photo$ = this.photoService.findById(id);        
+        this.photo$ = this.photoService.findById(id);
+        this.photo$.subscribe(() => {}, () => {
+            this.router.navigate(['not-found']);
+        });      
     }
 
     remove() {
         this.photoService.removePhoto(this.photoId)
-            .subscribe(() => this.router.navigate(['']));
+            .subscribe(
+                () => { 
+                    this.alertService.success('Photo removed', true);
+                    this.router.navigate(['/user', this.userService.getUserName()]);
+                },
+                (erro) => {
+                    this.alertService.warning('Could not delete the photo');
+                });
     }
 }
---
+++ alurapic/src/app/shared/components/alert/alert.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { AlertComponent } from './alert.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    declarations: [
+        AlertComponent
+    ],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        AlertComponent
+    ]
+})
+export class AlertModule { }
