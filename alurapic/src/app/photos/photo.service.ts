62327c0f6391d830a6f814da44b33dbfb02ace4b
Luiz Gustavo <oluizgustavob@gmail.com>
2019-12-04 08:12:31 -0300

    usando environment

--- alurapic/src/app/photos/photo.service.ts
+++ alurapic/src/app/photos/photo.service.ts
@@ -4,8 +4,9 @@ import { Observable, of, throwError } from 'rxjs';
 import { Photo } from './photo/photo';
 import { PhotoComment } from './photo/photo-comment';
 import { map, catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
-const API_URL = 'http://localhost:3000/'
+const API_URL = environment.api_url;
 
 @Injectable({
   providedIn: 'root'
@@ -15,12 +16,12 @@ export class PhotoService {
     constructor(private http: HttpClient) { }
 
     listFromUser(user: string): Observable<Photo[]> {
-      return this.http.get<Photo[]>(API_URL + user + '/photos');
+      return this.http.get<Photo[]>(API_URL + '/' + user + '/photos');
     }
 
     listFromUserPaginated(user: string, page: number) : Observable<Photo[]> {
       const params = new HttpParams().append('page', page.toString());
-      return this.http.get<Photo[]>(API_URL + user + '/photos', { params } );
+      return this.http.get<Photo[]>(API_URL + '/' + user + '/photos', { params } );
     }
 
     upload(description: string, allowComments: boolean, file: File) {
@@ -28,27 +29,27 @@ export class PhotoService {
       formData.append('description', description);
       formData.append('allowComments', allowComments ? 'true' : 'false');
       formData.append('imageFile', file);
-      return this.http.post(API_URL + 'photos/upload', formData);
+      return this.http.post(API_URL + '/photos/upload', formData);
     }
 
     findById(id: number) {
-      return this.http.get<Photo>(API_URL + 'photos/' + id);
+      return this.http.get<Photo>(API_URL + '/photos/' + id);
     }
 
     getComments(id: number) {
-      return this.http.get<PhotoComment[]>(API_URL + 'photos/' + id + '/comments');
+      return this.http.get<PhotoComment[]>(API_URL + '/photos/' + id + '/comments');
     }
 
     addComment(id: number, comment: string){
-      return this.http.post( API_URL + 'photos/' + id + '/comments', { commentText: comment } );
+      return this.http.post( API_URL + '/photos/' + id + '/comments', { commentText: comment } );
     }
 
     removePhoto(id: number) {
-      return this.http.delete( API_URL + 'photos/' + id);
+      return this.http.delete( API_URL + '/photos/' + id);
     }
 
     like(id: number) {
-      return this.http.post(API_URL + 'photos/' + id + '/like', {}, {observe: 'response'})
+      return this.http.post(API_URL + '/photos/' + id + '/like', {}, {observe: 'response'})
         .pipe( map(resp => true) )
         .pipe( catchError(err => {
           return err.status = '304' ? of(false) : throwError(err);
--- alurapic/src/environments/environment.prod.ts
+++ alurapic/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  api_url: 'http://ip-do-servidor:3000'
 };
