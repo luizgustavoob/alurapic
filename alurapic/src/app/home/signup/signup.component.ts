2337808382746ac3c76d191db0689749b9af3dd9
Luiz Gustavo <oluizgustavob@gmail.com>
2019-12-24 11:31:35 -0300

    validação de formulário

--- alurapic/src/app/home/signup/signup.component.ts
+++ alurapic/src/app/home/signup/signup.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { NewUser } from './new-user';
+
+import { SignUpService } from './signup.service';
+
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
-import { SignUpService } from './signup.service';
-import { NewUser } from './new-user';
-import { Router } from '@angular/router';
+import { userNamePassword } from './username-password.validator';
 import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
 
 @Component({
@@ -54,6 +58,8 @@ export class SignUpComponent implements OnInit {
                     Validators.maxLength(14)
                 ]
             ]
+        }, {
+            validator: userNamePassword
         });
 
         this.platformDetectorService.isPlatformBrower &&
@@ -61,6 +67,10 @@ export class SignUpComponent implements OnInit {
     }
 
     signup() {
+        if (this.signupForm.invalid || this.signupForm.pending) {
+            return;
+        }
+
         const newUser = this.signupForm.getRawValue() as NewUser;
         this.signupService.signup(newUser)
             .subscribe(resposta => {
